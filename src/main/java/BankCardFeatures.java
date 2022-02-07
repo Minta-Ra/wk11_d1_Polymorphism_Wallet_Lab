52a1e9210d5d6d06fd4e545b5d1e89cc788ea3a1
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-02-07 15:31:06 +0000

    Finished lab

---
+++ src/main/java/BankCardFeatures.java
@@ -0,0 +1,24 @@
+public abstract class BankCardFeatures {
+
+    private String cardNumber;
+    private String expiryDate;
+    private int securityNumber;
+
+    public BankCardFeatures(String cardNumber, String expiryDate, int securityNumber){
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.securityNumber = securityNumber;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public int getSecurityNumber() {
+        return securityNumber;
+    }
+}
